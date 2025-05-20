3127fae77fc99a8dd6839bf142d32a5219c57a47
Vikas <vikasmirashi2005@gmail.com>
2025-05-20 13:27:32 +0530

    Update ml8.py

--- ml8.py
+++ ml8.py
@@ -5,15 +5,11 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
-df=pd.read_csv('Boston.csv')
-df.head(3)
-X = df.drop(columns = ['medv'],axis=1)
-y=df['medv']
+df=pd.read_csv('/content/Iris.csv')
+X = df.drop(columns = ['Species'],axis=1)
+y=df['Species']
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
-print(X_train.shape) 
-print(y_train.shape) 
-print(X_test.shape) 
-print(y_test.shape) 
+
 model = LinearRegression()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
@@ -22,5 +18,5 @@ plt.xlabel('Actual Values')
 plt.ylabel('Predicted Values')
 plt.title('Actual vs Predicted Values')
 plt.show()
-print('Root Mean Squared error:(RMSE)',np.sqrt (mean_squared_error(y_test,y_pred)))
+print('Mean Squared error:', (mean_squared_error(y_test,y_pred)))
 print('R2-Square:',r2_score(y_test,y_pred))
